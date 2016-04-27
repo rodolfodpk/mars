e86ed037578ccf00b8c392691111c124ece2ed17
rodolfo.dpk <rodolfo_dpk@yahoo.com>
2016-04-27 13:54:45 -0300

    test method name style

--- mars-core/src/test/java/com/rdpk/mars/LocationTests.java
+++ mars-core/src/test/java/com/rdpk/mars/LocationTests.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 public class LocationTests {
 	
 	@Test(expected = IllegalArgumentException.class)
-	public void testInvalidLocation() {
+	public void must_reject_on_invalid_location() {
 		
 		@SuppressWarnings("unused")
 		Location location = new Location(0, -1);
