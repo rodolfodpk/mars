77b1337c92534d487dd879956c7b917453c38990
rodolfodpk <rodolfo_dpk@yahoo.com>
2016-04-28 00:18:44 -0300

    web ctx listener

---
+++ mars-servlet/src/main/java/com/rdpk/mars/web/MarsMissionCtxListener.java
@@ -0,0 +1,32 @@
+package com.rdpk.mars.web;
+
+import org.apache.camel.CamelContext;
+import org.apache.camel.impl.DefaultCamelContext;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class MarsMissionCtxListener implements ServletContextListener {
+
+    CamelContext camel ;
+
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        this.camel = new DefaultCamelContext();
+        try {
+            camel.addRoutes(new MarsMissionRestRoute());
+            camel.start();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+        try {
+            camel.stop();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
